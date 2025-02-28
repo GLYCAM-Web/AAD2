7471d2046cc8817a57692a56df6aeb0da6ee8eac
B. Lachele Foley <lachele@gmail.com>
2025-02-28 02:33:55 -0500

    partway through significant mods to pose_rmsd_mask.c

--- 5.analyze_results/pose_rmsd_mask.c
+++ 5.analyze_results/pose_rmsd_mask.c
@@ -1,22 +1,25 @@
 #include <glylib.h>
 
+typedef struct {
+        int resNum; ///< The number assigned to the residue inside the file
+        int nA; ///< Number of atoms in the ring
+        char **Name;///< Names of the atoms in the ring
+	coord_3D *x; ///< 3D coordinates for each ring atom
+} ring_set; ///< A set of info about a ring
+
 void get_rmsd(char *reffile,char* allmodels,char *ringatomsfile){
-
-
-/*if(argc<4 || argc>4){
-	printf("USAGE ./get_rmsd reference.pdb allmodels.pdb ringatomsfile\n");
-	}*/
 
 fileset Cset,Pset,Oset;
 assembly *r;
 int i,j,k,l=0,m=0,n=0,test=0;
 
 //printf("just inside get_rmsd\n");
+
 Cset.N=strdup(reffile);
 r=load_pdb(Cset.N);
 //printf("just loaded %s The number of residues in this molecule are %d\n",reffile,r[0].m[0][0].nr);
-coord_3D ref[r[0].m[0][0].nr*6];
-int ref_size=0;
+//coord_3D ref[r[0].m[0][0].nr*6];
+//int ref_size=0;
 
 fileslurp Islurp;
 fileset Iset;
@@ -27,64 +30,119 @@ j=0;
 k=0;
 char *atom;
 atom=(char*)calloc(4,sizeof(char));
-char **names;
-names=(char**)calloc(r[0].m[0][0].nr*6,sizeof(char*));
-int numrings=0;
-int *ringsize;
-ringsize=(int*)calloc(r[0].m[0][0].nr,sizeof(int));
+//char **names;
+//names=(char**)calloc(r[0].m[0][0].nr*6,sizeof(char*));
+//int numrings=0;
+//int *ringsize;
+//ringsize=(int*)calloc(r[0].m[0][0].nr,sizeof(int));
 coord_3D **ring;
 ring=(coord_3D**)calloc(6,sizeof(coord_3D*));
+
+//printf("starting on the ring sets\n");
+ring_set *RS;
+int nRings=0;
+//printf("Counting the number of residues with rings\n");
+i=0;
 while(i<Islurp.n){
-        if(strstr(Islurp.L[i],"ATOM") != NULL){
-                i++;
+        if(strstr(Islurp.L[i],"RES") != NULL){ 
+		i++; 
+		nRings++;
+	}
+	i++;
+}
+RS = (ring_set*)calloc(nRings,sizeof(ring_set));
+//printf("Counting the number of atoms in each of the %d ring(s)\n", nRings);
+i=0;
+int nr=0;
+int nAtoms=0;
+while(i<Islurp.n){
+	nAtoms=0;
+        if(strstr(Islurp.L[i],"RES") != NULL){ 
+		i++; 
                 while(strstr(Islurp.L[i],"END") == NULL){
-                        //if(j>0) printf("1.  names[%d] is >>>%s<<<\n",j-1,names[j-1]);
-                        sscanf(Islurp.L[i],"%s",atom);
-                        names[j]=(char*)calloc(strlen(atom),sizeof(char));
-                        names[j]=strdup(atom);
-                        //printf("2.  names[%d] is >>>%s<<<\n",j,names[j]);
+			i++;
+			nAtoms++;
+		}
+                RS[nr].nA=nAtoms;
+		RS[nr].Name=(char**)calloc(nAtoms,sizeof(char*));
+		RS[nr].x=(coord_3D*)calloc(nAtoms,sizeof(coord_3D)); ///< 3D coordinates for each ring atom
+	        nr++;
+	}
+	i++;
+}
+
+
+i=0;
+nr=0;
+int na=0;
+char dum[50];
+while(i<Islurp.n){
+        if(strstr(Islurp.L[i],"RES") != NULL){
+		//printf("Islurp.L[%d] is >>>%s<<<\n",i,Islurp.L[i]);
+		sscanf(Islurp.L[i],"%s %d",dum,&RS[nr].resNum);
+                //printf("dum is >>>%s<<< and RS[nr].resNum is %d\n",dum, RS[nr].resNum);
+                i++;
+		na=0;
+                while(strstr(Islurp.L[i],"END") == NULL){
+			//printf("Islurp.L[%d] is >>>%s<<<\n",i,Islurp.L[i]);
+                        sscanf(Islurp.L[i],"%s",dum);
+			RS[nr].Name[na]=strdup(dum);
+                	//printf("dum is >%s< and RS[%d].Name[%d] is >>>%s<<<\n",dum,nr,na, RS[nr].Name[na]);
                         i++;
-                        j++;
-			ringsize[numrings]++;
+			na++;
                 }
-	        numrings++;
+		nr++;
         }
         i++;
 }
 
-
-for(i=0;i<j;i++){
-	//printf("names are %s\n",names[i]);
+int nrc=0; // comparison ring counter
+for(nr=0;nr<nRings;nr++){
+	/**
+	printf("Residue number  %d  has these atoms:  \n",RS[nr].resNum);
+	for(na=0;na<RS[nr].nA;na++){
+		printf("   >>>%s<<< \n",RS[nr].Name[na]);
+	}
+	**/
+	for(nrc=nr+1; nrc<nRings; nrc++){ 
+		if(RS[nr].resNum == RS[nrc].resNum){
+			printf("ERROR: In get_rmsd, duplicate residue numbers are not allowed in the glycan.\n");
+			printf("       Residues with indexes %d and %d are both called number %d.\n",nr, nrc, RS[nrc].resNum);
+			exit(1);
+		}
+	}
 }
 
 //printf("done with part 1\n");
+START HERE with mods and diffs and all those many counters
+
 i=0;
 j=0;
 k=0;
 int check=0;
-int ringnumber=0;
+//int ringnumber=0;
 //printf("c.nr is %d\n",r[0].nm);
 //printf("atoms are %d\n",r[0].m[0][0].nr);
 for(i=0;i<r[0].m[0][0].nr;i++){
-	if(i>0 && check==1){
-		k=k+ringsize[ringnumber-1];
-	}
+//	if(i>0 && check==1){
+//		k=k+ringsize[ringnumber-1];
+//	}
 	//printf("k is %d\n",k);
-	n=k;
+//	n=k;
 	//printf("n is %d\n",n);
 	//printf("number of atoms are %d\n",r[0].m[0][0].r[i].na);	
-	if(r[0].m[0][0].r[i].na >= 6){
-	check=1;
+//	if(r[0].m[0][0].r[i].na >= 6){
+//	check=1;
 	//printf("the size of ring number %d is %d\n",ringnumber, ringsize[ringnumber]);
 	//for(test=0;test<6;test++)
 	for(test=0;test<ringsize[ringnumber];test++){
 	//printf("number of atoms are %d\n",r[0].m[0][0].r[i].na);
 	for(j=0;j<r[0].m[0][0].r[i].na;j++){
 		//printf("names[n=%d] is %s\n",n,names[n]);
-		if(strcmp(r[0].m[0][0].r[i].a[j].N,names[n]) == 0){
+if(strcmp(r[0].m[0][0].r[i].a[j].N,names[n]) == 0){
 		//printf("%s\n",r[0].m[0][0].r[i].a[j].N);
 		//printf("ref_size is %d \n",ref_size);
-		ref[ref_size]=r[0].m[0][0].r[i].a[j].x;
+ref[ref_size]=r[0].m[0][0].r[i].a[j].x;
 		ref_size++;		
 		}
 	}
@@ -226,6 +284,5 @@ for(i=0;i<A[0].nm;i++){
 
 fclose(Oset.F);
 //printf("Look for out_rmsd.txt file for results.\nThe output file is usually located in the same folder from which you run this program\n");
-
 
 }
